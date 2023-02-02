8cbad625d352bb84fc420629e4cad7108147d050
daniel <squaredfreezer@gmail.com>
2023-02-01 22:37:38 -0500

    add back-end API calls to get basic user info, champion mastery data, and user ranked data.

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ require('dotenv').config()
 const API_KEY = process.env.API_KEY
 
 const handleRegionNumber = (region) => {
-    const add1 = ['br', 'eune', 'euw', 'jp', 'lan', 'na', 'oce', 'tr']
+    if(region == 'oce') return 'oc1'
+    const add1 = ['br', 'eune', 'euw', 'jp', 'lan', 'na', 'tr']
     const add2 = ['las', 'ph', 'sg', 'th', 'tw', 'vn']
     if(add1.includes(region)) return region.concat("1")
     if(add2.includes(region)) return region.concat("2")
     return region
 }
-
+const handleRegionArea = (region) => {
+    if(region == 'br' || region == 'lan' || region == 'na' || region == 'las') return 'americas'
+    if(region == 'eune' || region == 'euw' || region == 'tr' || region == 'ru') return 'europe'
+    if(region == 'jp' || region == 'kr' || region == 'oce') return 'asia'
+    if(region == 'ph' || region == 'sg' || region == 'tw' || region == 'th' || region == 'vn') return 'sea'
+}
+const getData = async (url) => {
+    try {
+        const response = await axios.get(url)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
@@ -21,18 +35,39 @@ app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
 app.get('/data', (req, res) => {
+    const regionArea = handleRegionArea(req.query.region)
     const region = handleRegionNumber(req.query.region)
-    const summonerName = req.query.summonerName
+    const summonerName = req.query.summoner_name
+    let ret = {}
+    const getSummonerData = async (summonerName, region) => {
+        
+        axios.defaults.headers['X-Riot-Token'] = API_KEY
+        axios.defaults.baseURL = `https://${region}.api.riotgames.com/lol`
 
-    axios.get(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
-        headers: {
-            "X-Riot-Token": API_KEY
+        const basicInfo = await getData(`/summoner/v4/summoners/by-name/${summonerName}`)
+        ret.region = region
+        ret.summonerName = summonerName
+        ret.summonerId = basicInfo.id
+        ret.profileIconId = basicInfo.profileIconId
+        ret.summonerLevel = basicInfo.summonerLevel
+        ret.puuid = basicInfo.puuid
+
+        const rankedInfo = await getData(`/league/v4/entries/by-summoner/${ret.summonerId}`)
+        for(let i = 0 ; i < rankedInfo.length; i++) {
+            if(rankedInfo[i].queueType == 'RANKED_SOLO_5x5') ret.soloStats = rankedInfo[i]
+            if(rankedInfo[i].queueType = 'RANKED_FLEX_SR') ret.flexStats = rankedInfo[i]
         }
-    })
-    .then(response => {
-        res.send(response.data)
-    })
-    .catch(error => {
-        res.send(error)
+        const masteryInfo = await getData(`/champion-mastery/v4/champion-masteries/by-summoner/${ret.summonerId}`)
+        ret.masteryData = masteryInfo.map(champion => ({champion : champion.championId, level : champion.championLevel, 
+        points : champion.championPoints, pointsUntilNext : champion.championPointsUntilNextLevel}))
+
+        const matchIds = await getData(`https://${regionArea}.api.riotgames.com/lol/match/v5/matches/by-puuid/${ret.puuid}/ids`)
+        ret.matchIds = matchIds
+
+        return ret
+    }
+    getSummonerData(summonerName, region).then(result => {
+        res.send(result)
     })
 })
+// id, profileIconId, summonerLevel
